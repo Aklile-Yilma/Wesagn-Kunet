2055cc6eee1110615f4deac7b349ed329171ca1a
Abreham Atlaw <abrishatlaw@gmail.com>
2022-01-25 20:10:24 +0300

    Modified Login Redirect URL

--- src/main/java/com/gov/wesagnkunet/lib/auth/controllers/handlers/WesagnKunetAuthenticationSuccessHandler.java
+++ src/main/java/com/gov/wesagnkunet/lib/auth/controllers/handlers/WesagnKunetAuthenticationSuccessHandler.java
@@ -24,7 +24,7 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 public class WesagnKunetAuthenticationSuccessHandler implements AuthenticationSuccessHandler{
 
 	private Map<String, String> redirectionMap = new HashMap<String, String>(){{
-		put("ROLE_"+Role.ADMIN.name(), "/admin/dashboard");
+		put("ROLE_"+Role.ADMIN.name(), "/admin/dashboard/requests/birth");
 		put("ROLE_"+Role.CLIENT.name(), "/");
 	}};
 
